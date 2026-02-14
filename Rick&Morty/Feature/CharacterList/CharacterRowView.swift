2fb2ec052efe4927be38bd2b7a9bab45cda526ce
Jeddawy <ielgeddawy@gmail.com>
2026-02-14 15:32:24 +0200

    Create CharacterRowView

---
+++ Rick&Morty/Feature/CharacterList/CharacterRowView.swift
@@ -0,0 +1,36 @@
+//
+//  CharacterRowView.swift
+//  Rick&Morty
+//
+//  Created by Ibrahim El-geddawy on 14/02/2026.
+//
+
+
+import SwiftUI
+
+struct CharacterRowView: View {
+    let character: CharacterModel
+    
+    var body: some View {
+        HStack {
+            NetworkImageView(url: character.imageUrl)
+                .frame(width: 80, height: 80)
+                .cornerRadius(8)
+                .clipped()
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(character.name)
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                
+                Text(character.species)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.leading, 8)
+            
+            Spacer()
+        }
+        .padding(.vertical, 4)
+    }
+}
