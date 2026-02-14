62abc61ae0d1740a8ab8958ec743e1c7bc587039
Jeddawy <ielgeddawy@gmail.com>
2026-02-14 11:51:48 +0200

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Ibrahim El-geddawy on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rick&MortyTests/Rick_MortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyTests.swift
+//  Rick&MortyTests
+//
+//  Created by Ibrahim El-geddawy on 14/02/2026.
+//
+
+import Testing
+@testable import Rick_Morty
+
+struct Rick_MortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
