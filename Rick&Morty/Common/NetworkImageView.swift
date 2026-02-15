dd12277ff49a60b60edc35e13a8b95b78ed9f6e7
Jeddawy <ielgeddawy@gmail.com>
2026-02-15 13:16:07 +0200

    Claenup NetwrokImageView

--- Rick&Morty/Common/NetworkImageView.swift
+++ Rick&Morty/Common/NetworkImageView.swift
@@ -7,45 +7,38 @@
 
 import SwiftUI
 
-//MARK: TODO handle the arge amount of llist
-
 struct NetworkImageView<Placeholder: View>: View {
     let url: URL?
     let contentMode: ContentMode
     let fallbackSystemName: String
     let placeholder: Placeholder
-    let animation: Animation?
 
     init(
         url: String,
         contentMode: ContentMode = .fill,
         fallbackSystemName: String = AppImages.personFill,
-        @ViewBuilder placeholder: () -> Placeholder = { Color.gray.opacity(0.1) },
-        animation: Animation? = .easeInOut(duration: 0.3)
+        @ViewBuilder placeholder: () -> Placeholder = { Color.gray.opacity(0.1) }
     ) {
         self.url = URL(string: url)
         self.contentMode = contentMode
         self.fallbackSystemName = fallbackSystemName
         self.placeholder = placeholder()
-        self.animation = animation
     }
 
     init(
         url: URL?,
         contentMode: ContentMode = .fill,
         fallbackSystemName: String = AppImages.personFill,
-        @ViewBuilder placeholder: () -> Placeholder = { Color.gray.opacity(0.1) },
-        animation: Animation? = .easeInOut(duration: 0.3)
+        @ViewBuilder placeholder: () -> Placeholder = { Color.gray.opacity(0.1) }
     ) {
         self.url = url
         self.contentMode = contentMode
         self.fallbackSystemName = fallbackSystemName
         self.placeholder = placeholder()
-        self.animation = animation
     }
 
     var body: some View {
-        AsyncImage(url: url, transaction: Transaction(animation: animation)) { phase in
+        AsyncImage(url: url) { phase in
             switch phase {
             case .empty:
                 ZStack { placeholder; ProgressView() }
